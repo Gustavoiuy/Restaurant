e9ab69fe5cae6bf1fdbfe8f12893f245cca3f1ca
Gustavoiuy <20200971@uthh.edu.mx>
2025-01-22 07:16:42 -0600

    Pagination html

--- README.md
+++ README.md
@@ -6,10 +6,10 @@
 
 1. Clonar el repositorio
 2. Instalar dependencias ```npm install```
-3. Correr el proyecto    ```npm run dev```
-
-
-
+3. levantar la base de datos ```docker compose up -d```
+4. Correr las migraciones ```npx prisma migrate dev```
+5. ejecutar seed ```npm run seed```
+6. Correr el proyecto    ```npm run dev```
 
 
 
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,12 +1,31 @@
-import { ProductGrid, Title } from '@/components';
+import { ProductGrid, Title , Pagination} from '@/components';
 import { initialData } from '@/seed/seed';
+import { Product } from '../../interfaces/product.interface';
+import { getPaginatedProductsWithImages } from '@/actions';
+import { redirect } from 'next/navigation';
 
 
-const products = initialData.products;
+
+interface Props{
+  searchParams: {
+    page?: string;
+  }
+}
+
+export default async function Home({searchParams}:Props) {
+
+  const page = searchParams.page ? parseInt( searchParams.page) : 1;
 
 
+  const {products,currentPage, totalPages} = await getPaginatedProductsWithImages({
+    page
+  });
 
-export default function Home() {
+  if( products.length === 0){
+    redirect('/');
+  }
+
+
   return (
     <>
       <Title
@@ -18,7 +37,7 @@ export default function Home() {
       <ProductGrid 
         products={ products }
       />
-      
+      <Pagination totalPages={totalPages}/>
     </>
   );
 }
